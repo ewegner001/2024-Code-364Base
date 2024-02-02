ec480cd6d60c37337124d06c675b92e30d4fe3eb
leo <tulcanazal@tmore.org>
2024-02-01 19:45:02 -0600

    Shooter pivot feedforward control put with PIDControl

--- src/main/java/frc/robot/subsystems/ShooterPivot.java
+++ src/main/java/frc/robot/subsystems/ShooterPivot.java
@@ -11,21 +11,23 @@ import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ShooterPivot extends SubsystemBase {
   private CANSparkMax motor;
   private CANcoder cancoder;
   private double m_setPoint;
   private PIDController pid;
-  private ArmFeedforward feedforward;
+  private ArmFeedforward ff;
   /** Creates a new ShooterPiviot. */
   public ShooterPivot() {
     motor = new CANSparkMax(1, MotorType.kBrushless);
     cancoder = new CANcoder(0);
     m_setPoint = 0;
     pid = new PIDController(0, 0, 0);
-    feedforward = new ArmFeedforward(0, 0, 0, 0);
+    this.ff = new ArmFeedforward(0, 0, 0, 0);
 
   }
 
@@ -39,18 +41,24 @@ public class ShooterPivot extends SubsystemBase {
     m_setPoint = setPoint;
   }
 
-  public void feedForwardShooterpivot(double motorVelocity) {
-    
+  private double encoderPosition() {
+    StatusSignal<Double> aPosition = cancoder.getAbsolutePosition();
+    Double aPositionValue = aPosition.getValue();
+    return aPositionValue.doubleValue();
   }
- 
- 
+
+  private double encoderVelocity() {
+    StatusSignal<Double> velocity = cancoder.getVelocity();
+    Double velocityValue = velocity.getValue();
+    return velocityValue.doubleValue();
+  }
  
  
  
   @Override
   public void periodic() {
-    motor.set(pid.calculate(encoderAngle(), m_setPoint));
-    motor.setVoltage(feedforward.calculate(motorVelocity));
+    double feedForward = ff.calculate(Units.degreesToRadians(encoderPosition()), Units.degreesToRadians(encoderVelocity()));
+    motor.setVoltage(pid.calculate(encoderAngle(), m_setPoint) + feedForward);
     // This method will be called once per scheduler run
   }
 }
