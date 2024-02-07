6df1f0d36c2047e81768c1b48bef45cbe94bd694
leo <tulcanazal@tmore.org>
2024-02-06 19:49:57 -0600

    Motion profile shooter shooting motors started(change to velocity control)

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,9 +4,13 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -15,14 +19,25 @@ public class Shooter extends SubsystemBase {
   private TalonFX rightShooterMotor;
   private TalonFX frontShooterMotor;
   private DigitalInput inputSensor;
-  private PIDController pidAdvanced;
+  private Slot0Configs slotConfigs;
+  private TrapezoidProfile motorProfile;
+  private TrapezoidProfile.State motorGoal;
   /** Creates a new Shooter. */
   public Shooter() {
     leftShooterMotor = new TalonFX(0);
     rightShooterMotor = new TalonFX(0);
     frontShooterMotor = new TalonFX(0);
     inputSensor = new DigitalInput(0);
-    pidAdvanced = new PIDController(0, 0, 0);
+    slotConfigs = new Slot0Configs();
+    slotConfigs.kS = 0.05;
+    slotConfigs.kV = 0.12; 
+    slotConfigs.kA = 0.01;
+    slotConfigs.kP = 0.11; 
+    slotConfigs.kI = 0;
+    slotConfigs.kD = 0;
+    motorProfile = new TrapezoidProfile(
+      new TrapezoidProfile.Constraints(400, 4000)
+    );
     
   }
 
@@ -44,9 +59,9 @@ public class Shooter extends SubsystemBase {
     return inputSensor.get();
   }
 
-  public void shootingMotorsController() {
-    leftShooterMotor
-
+  public void shootingMotorsConfig() {
+    rightShooterMotor.getConfigurator().apply(slotConfigs);
+    rightShooterMotor.setControl()
   }
 
   
