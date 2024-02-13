71df11468d50891700b47c748a20036db73db10e
leo <tulcanazal@tmore.org>
2024-02-12 19:59:30 -0600

    Intake starter code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,26 +4,49 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.REVLibError;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-  private CANSparkMax motor;
+  private final double intakePivotMotorGearRatio = 100.0;
+  private final int intakeMotorID = 12;
+  private final int intakePivotID = 11;
+  private final int intakePivotEncoderID = 1;
+
+  private CANSparkMax intakeMotor;
+  private CANSparkMax m_IntakePiviot;
+  private CANcoder intakePivotEncoder;
+  private RelativeEncoder intakePivotMotorEncoder;
+  private PIDController pid;
 
   /** Creates a new Intake. */
   public Intake() {
-    motor = new CANSparkMax(12, MotorType.kBrushless);
+    intakeMotor = new CANSparkMax(intakeMotorID, MotorType.kBrushless);
+    m_IntakePiviot = new CANSparkMax(intakePivotID, MotorType.kBrushless);
+    intakePivotEncoder = new CANcoder(intakePivotEncoderID);
+    intakePivotMotorEncoder = m_IntakePiviot.getEncoder();
+    intakePivotMotorEncoder.setPositionConversionFactor(360 / intakePivotMotorGearRatio);
+    intakePivotMotorEncoder.setPosition(intakePivotEncoder.getPosition().getValue());
+    pid = new PIDController(intakePivotID, intakePivotEncoderID, intakeMotorID);
   }
 
-  public void run(double speed) {
-    motor.set(speed);
+  private void intakePivotMotorPID() {
+    
   }
 
-  public double intakeMotorTemp() {
-    return motor.getMotorTemperature();
-  }
+
+
+
+
+  
+
+  
 
   // @Override
   // public void periodic() {
