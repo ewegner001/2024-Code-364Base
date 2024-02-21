9910d020f83bb347cf83fea5884fcd0b0043b7a8
leo <tulcanazal@tmore.org>
2024-02-20 18:46:18 -0600

    swerve drive max speed constant changed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -99,7 +99,7 @@ public final class Constants {
         /* Swerve Profiling Values */
         /** Meters per Second */
         //4.5
-        public static final double maxSpeed = 4.5*0.8; //TODO: This must be tuned to specific robot
+        public static final double maxSpeed = 5.11; //TODO: This must be tuned to specific robot
         /** Radians per Second */
         public static final double maxAngularVelocity = 10.0; //TODO: This must be tuned to specific robot
 
