69a43bfcc05fce1445ed67c3250e58bf55bfc580
ewegner001 <88629283+ewegner001@users.noreply.github.com>
2024-04-01 01:24:51 -0500

    Target Logging, Alliance protection, minor error changes

--- src/main/java/frc/robot/commands/GoToTrap.java
+++ src/main/java/frc/robot/commands/GoToTrap.java
@@ -10,6 +10,11 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.Eyes;
@@ -19,6 +24,8 @@ public class GoToTrap extends Command {
     Eyes s_Eyes;
     Swerve s_Swerve;
 
+    private StructPublisher<Translation2d> translationPublisher;
+
     public double robotX = 0.0;
     public double robotY = 0.0;
     public double robotR = 0.0;
@@ -27,10 +34,10 @@ public class GoToTrap extends Command {
     public double targetY = 0.0;
     public double targetR = 0.0;
 
-    public double closestDistance = 0.0;
     public double positionTolerance = 0.1;
     public double rotationTolerance =  2.0;
     public double distanceLimit = 3.0;
+    public double closestDistance = distanceLimit; 
 
     // blue trap positions
     public double blueTrapLeftX = 4.3;
@@ -75,6 +82,8 @@ public class GoToTrap extends Command {
     @Override
     public void initialize() {
 
+        translationPublisher = NetworkTableInstance.getDefault().getStructTopic("/Closest Trap", Translation2d.struct).publish();
+
         robotX = s_Eyes.getRobotPose().getX();
         robotY = s_Eyes.getRobotPose().getY();
         robotR = s_Eyes.getRobotPose().getRotation().getDegrees();
@@ -96,39 +105,46 @@ public class GoToTrap extends Command {
             redTrapCenterDistance
         };
 
-        closestDistance = blueTrapLeftDistance;
-
-        for(int i = 0; i < 6; i++) {
+        for(int i = 0; i < distances.length; i++) {
             if (closestDistance < distances[i]) {
                 closestDistance = distances[i];
             }
         }
-
-        if (closestDistance == blueTrapLeftDistance) {
-            targetX = blueTrapLeftX;
-            targetY = blueTrapLeftY;
-            targetR = blueTrapLeftR;
-        } else if (closestDistance == blueTrapRightDistance) {
-            targetX = blueTrapRightX;
-            targetY = blueTrapRightY;
-            targetR = blueTrapRightR;
-        } else if (closestDistance == blueTrapCenterDistance) {
-            targetX = blueTrapCenterX;
-            targetY = blueTrapCenterY;
-            targetR = blueTrapCenterR;
-        } else if (closestDistance == redTrapLeftDistance) {
-            targetX = redTrapLeftX;
-            targetY = redTrapLeftY;
-            targetR = redTrapLeftR;
-        } else if (closestDistance == redTrapRightDistance) {
-            targetX = redTrapRightX;
-            targetY = redTrapRightY;
-            targetR = redTrapRightR;
-        } else if (closestDistance == redTrapCenterDistance) {
-            targetX = redTrapCenterX;
-            targetY = redTrapCenterY;
-            targetR = redTrapCenterR;
+        
+        if (DriverStation.getAlliance().get() == Alliance.Blue) {
+            if (closestDistance == blueTrapLeftDistance) {
+                targetX = blueTrapLeftX;
+                targetY = blueTrapLeftY;
+                targetR = blueTrapLeftR;
+            } else if (closestDistance == blueTrapRightDistance) {
+                targetX = blueTrapRightX;
+                targetY = blueTrapRightY;
+                targetR = blueTrapRightR;
+            } else if (closestDistance == blueTrapCenterDistance) {
+                targetX = blueTrapCenterX;
+                targetY = blueTrapCenterY;
+                targetR = blueTrapCenterR;
+            }
+        } else if (DriverStation.getAlliance().get() == Alliance.Red) {
+            if (closestDistance == redTrapLeftDistance) {
+                targetX = redTrapLeftX;
+                targetY = redTrapLeftY;
+                targetR = redTrapLeftR;
+            } else if (closestDistance == redTrapRightDistance) {
+                targetX = redTrapRightX;
+                targetY = redTrapRightY;
+                targetR = redTrapRightR;
+            } else if (closestDistance == redTrapCenterDistance) {
+                targetX = redTrapCenterX;
+                targetY = redTrapCenterY;
+                targetR = redTrapCenterR;
+            }
+        } else {
+            closestDistance = distanceLimit;
         }
+
+        //Log Target Trap Location
+        translationPublisher.set(new Translation2d(targetX, targetY));
 
         List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(
 
@@ -137,14 +153,21 @@ public class GoToTrap extends Command {
 
         );
 
+
         PathPlannerPath path = new PathPlannerPath(
             bezierPoints,
             new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI),
             new GoalEndState(0.0, Rotation2d.fromDegrees(targetR))
         );
 
-        if (closestDistance < distanceLimit) {
+        // Prevent the path from being flipped as all coordinates are blue origin
+        path.preventFlipping = true;
+
+        if (closestDistance < distanceLimit) { //DO NOT INCLUDE DISTANCE LIMIT
             AutoBuilder.followPath(path);
+            SmartDashboard.putString("Trap Status", "Going to Trap");
+        } else{
+            SmartDashboard.putString("Trap Status", "Too Far Away To Trap");
         }
 
     }
@@ -163,7 +186,7 @@ public class GoToTrap extends Command {
     public boolean isFinished() {
 
         double xError = Math.abs(targetX + s_Swerve.m_poseEstimator.getEstimatedPosition().getX());
-        double yError = Math.abs(targetX + s_Swerve.m_poseEstimator.getEstimatedPosition().getY());
+        double yError = Math.abs(targetY + s_Swerve.m_poseEstimator.getEstimatedPosition().getY());
         double rError = Math.abs(targetR + s_Swerve.m_poseEstimator.getEstimatedPosition().getRotation().getDegrees() % 360);
 
         if (xError <= positionTolerance && yError <= positionTolerance && rError <= rotationTolerance) {
